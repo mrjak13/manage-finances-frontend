0002768d16e888186529af8121968412299ea51c
Justin Kenney <justinkenney13@gmail.com>
2019-06-28 07:55:06 -0400

    add getCurrentUser action

--- src/actions/currentUser.js
+++ src/actions/currentUser.js
@@ -28,6 +28,26 @@ export const login = credentials => {
 	}
 }
 
+export const getCurrentUser = () => {
+	return dispatch => {
+		return fetch("http://localhost:3005/api/v1/get_current_user", {
+			method: "GET",
+			headers: {
+				"Content-Type" : "application/json"
+			},
+		})
+			.then(res => res.json())
+			.then(user => {
+				if (user.error) {
+					alert(user.error)
+ 				} else {
+ 					dispatch(setCurrentUser(user))
+ 				}
+			})
+			.catch()
+	}
+}
+
 export const test = stuff => {
 	console.log("credentials are: ", stuff)
 	return dispatch => {
