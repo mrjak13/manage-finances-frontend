4a7279d4a956c419bfb7ab0f4269d8beeb4612e2
Justin Kenney <justinkenney13@gmail.com>
2019-06-28 08:34:21 -0400

    add credentials: "include" to enable sessions

--- src/actions/currentUser.js
+++ src/actions/currentUser.js
@@ -10,6 +10,7 @@ export const login = credentials => {
 
 	return dispatch => {
 		return fetch("http://localhost:3005/api/v1/login", {
+			credentials: "include",
 			method: "POST",
 			headers: {
 				"Content-Type" : "application/json"
@@ -31,6 +32,7 @@ export const login = credentials => {
 export const getCurrentUser = () => {
 	return dispatch => {
 		return fetch("http://localhost:3005/api/v1/get_current_user", {
+			credentials: "include",
 			method: "GET",
 			headers: {
 				"Content-Type" : "application/json"
