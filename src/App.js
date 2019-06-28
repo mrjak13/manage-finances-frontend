e06fedc2513b30b7b770539e924c8c10e4da80b4
Justin Kenney <justinkenney13@gmail.com>
2019-06-28 07:56:27 -0400

    changed App.js to class component, import connect and getCurrentUser

--- src/App.js
+++ src/App.js
@@ -2,14 +2,25 @@ import React from 'react';
 // import logo from './logo.svg';
 import './App.css';
 import Login from "./components/Login"
+import { connect } from 'react-redux'
+import { getCurrentUser } from './actions/currentUser'
 
-function App() {
-  return (
-    < Login / >
-  );
+class App extends React.Component {
+
+  componentDidMount() {
+    this.props.getCurrentUser()
+  }
+
+  render(){
+    return (
+      < Login / >
+    );
+
+  }
+
 }
 
-export default App;
+export default connect(null, { getCurrentUser })(App)
     // <div className="App">
     //   <header className="App-header">
     //     <img src={logo} className="App-logo" alt="logo" />
