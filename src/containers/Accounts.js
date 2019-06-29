3a1431a0057d38d30e9180f6232b6af20f42dbc0
Justin Kenney <justinkenney13@gmail.com>
2019-06-29 12:10:38 -0400

    on mount sets store to have accounts/creates account components for each

--- src/containers/Accounts.js
+++ src/containers/Accounts.js
@@ -1,25 +1,33 @@
 import React from 'react'
 import { connect } from 'react-redux'
+import { setAccounts } from '../actions/accounts' 
+import Account from '../components/Account'
 
 class Accounts extends React.Component {
 
 	handleClick = event => {
-			console.log(this.props.currentUser)
+			this.props.setAccounts(this.props.currentUser.attributes.accounts)
+	}
+
+	componentDidMount() {
+		this.props.setAccounts(this.props.currentUser.attributes.accounts)
 	}
 
 	render() {
 		return (
 			<div>
-				<button onClick={this.handleClick}>Accounts</button>
+
+				{ this.props.currentUser.attributes.accounts.map(account => <Account id={account.id} name={account.name} balance={account.balance}/>) }
 			</div>
 		)
 	}
 }
 
-const mapStateToProps = ({ currentUser }) => {
+const mapStateToProps = state => {
 	return {
-		currentUser
+		currentUser: state.currentUser.data,
+		accounts: state.accounts
 	}
 }
 
-export default connect(mapStateToProps)(Accounts)
+export default connect(mapStateToProps, { setAccounts })(Accounts)
