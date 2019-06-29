84449b5cc712cc89abfd34204f7a23231d5bda90
Justin Kenney <justinkenney13@gmail.com>
2019-06-29 12:08:53 -0400

    displays account info

--- src/components/Account.js
+++ src/components/Account.js
@@ -1,7 +1,11 @@
 import React from 'react'
 
-const Account = () => {
+const Account = ({ id, name, balance }) => {
 	return (
+		<div>
+			<h3>{ name }</h3>
+			<p>Account id {id} Balance {balance}</p> 
+		</div>
 	)
 }
 
