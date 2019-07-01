00524c77fdfe17666c7cbe700fa6fb51f7f64e2f
Justin Kenney <justinkenney13@gmail.com>
2019-07-01 13:56:33 -0400

    update navbar with bootstrap

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -2,16 +2,29 @@ import React from 'react'
 import { connect } from 'react-redux'
 import Login from "./Login"
 import Logout from "./Logout"
-// import Accounts from "../containers/Accounts"
+import Navbar from 'react-bootstrap/Navbar'
+import Nav from 'react-bootstrap/Nav'
+import NavDropdown from 'react-bootstrap/NavDropdown'
 
 const NavBar = ({ currentUser }) => {
 
 	return (
-		<div>
-			Welcome to managing your finances!
-			
-			{ currentUser.data ? < Logout /> : < Login/>}
-		</div>
+		<Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
+		  <Navbar.Brand href="#home">{ currentUser.data ? currentUser.data.attributes.name : "Manage Your Finances"}</Navbar.Brand>
+		  <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+		  <Navbar.Collapse id="responsive-navbar-nav">
+		    <Nav className="mr-auto">
+		      <Nav.Link href="#features">Features</Nav.Link>
+		      <Nav.Link href="#pricing">Pricing</Nav.Link>
+		      <NavDropdown title="Dropdown" id="basic-nav-dropdown">
+		        <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+		      </NavDropdown>
+		    </Nav>
+		    <Nav>
+		      { currentUser.data ? < Logout /> : < Login/> }
+		    </Nav>
+		  </Navbar.Collapse>
+		</Navbar>
 	)
 }
 
