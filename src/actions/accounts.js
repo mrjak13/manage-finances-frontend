c684153c1e5f0555c07873d7b0f90a322b8cffc8
Justin Kenney <justinkenney13@gmail.com>
2019-07-01 15:00:10 -0400

    start action to create account

--- src/actions/accounts.js
+++ src/actions/accounts.js
@@ -4,3 +4,18 @@ export const setAccounts = accounts => {
 		accounts
 	}
 }
+
+export const createAccount = (userId, account) => {
+	console.log("in createAccount action, account:", account, " and userId: ", userId)
+	return dispatch => {
+		return fetch(`http://localhost:3005/api/v1/users/${userId}/accounts`, {
+			credentials: "include",
+			method: "POST",
+			headers: {
+				"Content-Type" : "application/json"
+			}
+		})
+		.then(resp => resp.json())
+		.then(console.log("AFTER POST REQUEST"))
+	}
+}
