02cbd91d3653b363c072ff13ba3ea391637ea0d0
Justin Kenney <justinkenney13@gmail.com>
2019-07-03 10:19:50 -0400

    create signupform actions

---
+++ src/actions/signupForm.js
@@ -0,0 +1,12 @@
+export const updateSignupForm = formData => {
+	return {
+		type: "UPDATE_SIGNUP_FORM",
+		formData
+	}
+}
+
+export const clearSignupForm = () => {
+	return {
+		type: "CLEAR_SIGNUP_FORM"
+	}
+}
