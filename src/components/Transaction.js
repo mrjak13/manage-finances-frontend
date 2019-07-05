3d136153a2309b029792718887c302c89b394152
Justin Kenney <justinkenney13@gmail.com>
2019-07-05 13:55:47 -0400

    finish tranaction cards

--- src/components/Transaction.js
+++ src/components/Transaction.js
@@ -1,8 +1,19 @@
 import React from 'react'
+import { connect } from 'react-redux'
+import Card from 'react-bootstrap/Card'
 
-const Transaction = () => {
+
+const Transaction = ({ id, name, amount, action, date }) => {
 	return (
+			<Card style={{ width: '18rem' }}>
+		    <Card.Header>{ name }</Card.Header>
+			  <Card.Body>
+			  	<Card.Title className={(action === "Withdrawl" ? "text-danger" : "")}>{amount}</Card.Title>
+			    <Card.Subtitle className="mb-2 text-muted">Created on: {date}</Card.Subtitle>
+			    <Card.Subtitle className="mb-2 text-muted">Transaction ID: {id}</Card.Subtitle>
+			  </Card.Body>
+			</Card>
 	)
 }
 
-export default Transaction
+export default connect(null, {})(Transaction)
