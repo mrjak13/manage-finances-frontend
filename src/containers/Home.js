8c00390155c01e6d580f63a31980c3e93334954c
Justin Kenney <justinkenney13@gmail.com>
2019-07-05 12:33:25 -0400

    add a home page

---
+++ src/containers/Home.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import Container from 'react-bootstrap/Container'
+import Jumbotron from 'react-bootstrap/Jumbotron'
+
+
+const Home = (props) => {
+	return (
+		<Jumbotron fluid>
+		  <Container className="text-center">
+		  	<h5>Welcome to</h5>
+		    <h1>Managing your Finances</h1>
+		    <p>
+		      Here you can easily track your expenses and monitor your savings accross multiple accounts.
+		    </p>
+		  </Container>
+		</Jumbotron>
+	)
+}
+
+export default Home
