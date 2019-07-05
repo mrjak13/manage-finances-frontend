30235eef8556d592e84d1854f983d0da18150215
Justin Kenney <justinkenney13@gmail.com>
2019-07-05 13:50:02 -0400

    able to show transactions

--- src/containers/Transactions.js
+++ src/containers/Transactions.js
@@ -1,8 +1,33 @@
 import React from 'react'
+import { connect } from 'react-redux'
+import Transaction from '../components/Transaction'
+import CardColumns from 'react-bootstrap/CardColumns'
+import Container from 'react-bootstrap/Container'
 
-class Transaction extends React.Component {
+class Transactions extends React.Component {
+
+	renderTransactions = () => {
+		return this.props.transactions.map(transaction => {
+			return <Transaction id={transaction.id} name={transaction.attributes.name} amount={transaction.attributes.amount} action={transaction.attributes.action} date={transaction.attributes.date}/>
+		})
+	}
+	
 	render(){
 		return (
+			<Container>
+				<CardColumns>
+					{this.renderTransactions()}
+				</CardColumns>
+			</Container>
 		)
 	}
 }
+
+const mapStateToProps = state => {
+	return {
+		transactions: state.transactions
+	}
+}
+
+export default connect(mapStateToProps)(Transactions)
+					// {this.props.transactions.map(transaction => <Transaction name={transaction.attributes.name} amount={transaction.attributes.amount} action={transaction.attributes.action} date={transaction.attributes.date}/>)}
