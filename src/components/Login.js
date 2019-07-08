bf21786febfce2ea1022508fc7a83971f2aafa60
Justin Kenney <justinkenney13@gmail.com>
2019-07-08 15:54:03 -0400

    update to clear form on valid submission through new action

--- src/components/Login.js
+++ src/components/Login.js
@@ -20,10 +20,6 @@ const Login = ({ loginFormData, updateLoginForm, login}) => {
 	const handleSubmit = event => {
 		event.preventDefault()
 		login(loginFormData)
-		updateLoginForm({
-			email: "",
-			password: ""
-		})
 	}
 
 	return (
@@ -33,7 +29,7 @@ const Login = ({ loginFormData, updateLoginForm, login}) => {
 		      <Form.Control value={loginFormData.email} onChange={handleInputChange} name="email" placeholder="email@email.com" />
 		    </Col>
 		    <Col>
-		      <Form.Control value={loginFormData.password} onChange={handleInputChange} name="password" placeholder="password" />
+		      <Form.Control type="password" value={loginFormData.password} onChange={handleInputChange} name="password" placeholder="password" />
 		    </Col>
 			  <Button variant="outline-light" type="submit" value="Log In">Log In</Button>
 		  </Form.Row>
@@ -49,8 +45,3 @@ const mapStateToProps = state => {
 }
 
 export default connect(mapStateToProps, { updateLoginForm, login })(Login)
-		// <form onSubmit={handleSubmit}>
-		// 	<input type="text" value={loginFormData.email} onChange={handleInputChange} name="email" placeholder="email@email.com"/>
-		// 	<input type="text" value={loginFormData.password} onChange={handleInputChange} name="password" placeholder="password"/>
-		// 	<Button type="submit" value="Log In">Log In</Button>
-		// </form>
--- src/reducers/loginForm.js
+++ src/reducers/loginForm.js
@@ -7,6 +7,8 @@ export default (state = initialState, action) => {
 	switch (action.type) {
 			case "UPDATE_LOGIN_FORM":
 				return action.formData
+			case "CLEAR_LOGIN_FORM":
+				return initialState
 		default:
 			return state
 	}
