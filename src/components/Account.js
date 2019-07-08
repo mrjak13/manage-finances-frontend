78f68f8b4df5dd8f5a86bbefa42a95f751d83631
Justin Kenney <justinkenney13@gmail.com>
2019-07-08 17:55:22 -0400

    add bootstrap, button, delete function

--- src/components/Account.js
+++ src/components/Account.js
@@ -1,17 +1,25 @@
 import React from 'react'
 import { connect } from 'react-redux'
+import { deleteAccount } from '../actions/accounts'
 import { fetchTransactions } from '../actions/transactions'
 import { Link } from 'react-router-dom'
 import Card from 'react-bootstrap/Card'
+import Col from 'react-bootstrap/Col'
+import Row from 'react-bootstrap/Row'
 import Fade from 'react-reveal/Fade';
 
-const Account = ({ id, name, balance, fetchTransactions }) => {
+const Account = ({ id, name, balance, fetchTransactions, deleteAccount }) => {
 	return (
 		<Fade>
-			<Card style={{ width: '18rem' }}>
+			<Card style={{ width: '15rem' }}>
 			  <Card.Body>
-			    <Card.Title>{ name }</Card.Title>
-			    <Card.Subtitle className="mb-2 text-muted">Account id {id} Balance ${balance}</Card.Subtitle>
+		  		<Card.Title>
+				  	<Row>
+				  		<Col sm={9}>{ name }</Col>
+				  		<Col sm={3}><Link className="btn btn-outline-danger" onClick={() => deleteAccount(id)} variant="outline-danger" size="sm">X</Link></Col>
+				   	</Row>
+		  		</Card.Title>
+			    <Card.Subtitle className="mb-2 text-muted">Balance ${balance}</Card.Subtitle>
 			    <Link to={"/transactions"} onClick={() => fetchTransactions(id)}>Get Transactions</Link>
 			  </Card.Body>
 			</Card>
@@ -19,4 +27,4 @@ const Account = ({ id, name, balance, fetchTransactions }) => {
 	)
 }
 
-export default connect(null, { fetchTransactions })(Account)
+export default connect(null, { fetchTransactions, deleteAccount })(Account)
