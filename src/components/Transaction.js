bcffbc730bf8c4fd661f99a82a985f9a51c3b1cb
Justin Kenney <justinkenney13@gmail.com>
2019-07-08 18:55:12 -0400

    add bootstrap and delete function

--- src/components/Transaction.js
+++ src/components/Transaction.js
@@ -1,14 +1,23 @@
 import React from 'react'
+import { connect } from 'react-redux'
+import { deleteTransaction } from '../actions/transactions'
 import Card from 'react-bootstrap/Card'
+import { Link } from 'react-router-dom'
+import Col from 'react-bootstrap/Col'
+import Row from 'react-bootstrap/Row'
 import Fade from 'react-reveal/Fade';
 
 
-const Transaction = ({ id, name, amount, action, date }) => {
+const Transaction = ({ id, name, amount, action, date, deleteTransaction }) => {
 	return (
-		
 		<Fade>
 			<Card style={{ width: '14rem' }}>
-		    <Card.Header>{ name }</Card.Header>
+		    <Card.Header>
+		    	<Row>
+		    		<Col sm={9}>{ name }</Col>
+					  <Col sm={3}><Link className="btn btn-outline-danger" onClick={() => deleteTransaction(id)} variant="outline-danger" size="sm">X</Link></Col>
+					</Row>
+		    </Card.Header>
 			  <Card.Body>
 			  	<Card.Title className={(action === "Withdrawl" ? "text-danger" : "")}>${amount}</Card.Title>
 			    <Card.Subtitle className="mb-2 text-muted">Created on: {date}</Card.Subtitle>
@@ -16,8 +25,7 @@ const Transaction = ({ id, name, amount, action, date }) => {
 			  </Card.Body>
 			</Card>
 		</Fade>
-		
 	)
 }
 
-export default Transaction
+export default connect(null, { deleteTransaction })(Transaction)
